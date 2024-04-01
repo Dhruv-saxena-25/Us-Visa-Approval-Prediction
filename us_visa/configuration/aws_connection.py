a39755c18692ecd43452796188bfbd74ecf56457
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-04-01 15:10:29 +0100

    aws connection

--- us_visa/configuration/aws_connection.py
+++ us_visa/configuration/aws_connection.py
@@ -14,7 +14,7 @@ class S3Client:
         """
 
         if S3Client.s3_resource==None or S3Client.s3_client==None:
-            __access_key_id =  os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY, )
+            __access_key_id =  os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY, ) 
             __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY, )
             if __access_key_id is None:
                 raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not not set.")
